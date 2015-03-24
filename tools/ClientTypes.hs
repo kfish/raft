1239941b496d4f468e6f29ffecdc909abd8e7771
Conrad Parker <conrad@metadecks.org>
2015-03-24 17:09:39 +1100

    raft-client: add Binary instance for Cmd

--- tools/ClientTypes.hs
+++ tools/ClientTypes.hs
@@ -2,6 +2,9 @@ module ClientTypes (
     Cmd(..)
 ) where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+import Data.Binary
 import qualified Data.ByteString.Char8 as S
 
 data Cmd k v =
@@ -12,3 +15,25 @@ data Cmd k v =
   -- | CmdUse Host PortNumber
   -- | CmdPause
   -- | CmdDump
+
+instance (Binary k, Binary v) => Binary (Cmd k v) where
+    put (CmdSet k v) = do
+        put 'S'
+        put k
+        put v
+    put (CmdGet k) = do
+        put 'G'
+        put k
+    put (CmdSleep n) = do
+        put 'D'
+        put n
+    put _ = return ()
+
+    get = do
+        x <- get :: Get Char
+        case x of
+            'S' -> CmdSet <$> get <*> get
+            'G' -> CmdGet <$> get
+            'D' -> CmdSleep <$> get
+            _ -> mzero
+                
