b1e4661252383c1139b7e608ec240981381ff2a7
Conrad Parker <conrad@metadecks.org>
2015-03-30 04:33:51 +0000

    LogStoreF: add runTestStore and test

--- tests/TestStore.hs
+++ tests/TestStore.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -8,10 +9,15 @@ module TestStore (
   , testStore
   , testCommit
   , testTruncate
+
+  , testState
+  , runTestStore
 ) where
 
 import Data.Functor.Identity
 
+import Control.Monad.Free
+import Control.Monad.State
 import qualified Data.Foldable as Fold
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -55,3 +61,30 @@ testCommit ix ts = runIdentity $ CS.commit ix ts
 
 testTruncate :: CS.Index -> TestStore -> TestStore
 testTruncate ix ts = runIdentity $ CS.truncate ix ts
+
+----------------------------------------------------------------------
+
+testState :: Free (CS.LogStoreF [] Int) a -> CS.Index -> Maybe (Int, CS.Term)
+testState f ix = flip evalState empty $ do
+    runTestStore (f >> CS.end')
+    TestStore s _c <- get
+    return $ Map.lookup ix s
+
+runTestStore :: (MonadState TestStore m, Fold.Foldable t)
+             => Free (CS.LogStoreF t Int) () -> m ()
+runTestStore (Pure r) = return r
+runTestStore (Free x) = case x of
+    CS.LogQuery ix cont -> do
+        TestStore s c <- get
+        let res = Map.lookup ix s
+        runTestStore $ cont res
+    CS.LogStore ix term xs next -> do
+        modify $ \(TestStore s c) ->
+            (\m -> TestStore m c) . fst $ Fold.foldl' (\(m, ixx) x -> (Map.insert ixx (x,term) m, ixx+1)) (s, ix) xs
+    CS.LogCommit ix next -> do
+        modify $ \ts -> ts { tsLatestCommit = ix }
+        runTestStore next
+    CS.LogTruncate ix next -> do
+        modify $ \(TestStore s c) ->
+            TestStore (fst (Map.split ix s)) (min ix c)
+    CS.LogEnd -> return ()
