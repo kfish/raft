f4b08a8199c1728268af36a18a570cc337c89ff0
Conrad Parker <conrad@metadecks.org>
2015-05-19 18:49:32 +1000

    Cleanup

--- examples/TestStore.hs
+++ examples/TestStore.hs
@@ -7,13 +7,6 @@
 module TestStore (
     TestStore(..)
   , empty
-  -- , testQuery
-  -- , testStore
-  -- , testCommit
-  -- , testTruncate
-
-  -- , testState
-  , runTestStore
 ) where
 
 import qualified Data.ByteString.Char8 as S
@@ -31,7 +24,6 @@ import qualified Consensus.Types as CS
 ----------------------------------------------------------------------
 
 data TestStore = TestStore {
-      -- tsInternal :: Map CS.Index (S.ByteString, CS.Term)
       tsInternal :: Map CS.Index (Int, CS.Term)
     , tsLatestCommit :: CS.Index
     }
@@ -39,44 +31,7 @@ data TestStore = TestStore {
 empty :: TestStore
 empty = TestStore Map.empty 0
 
-{-
-instance CS.Store TestStore where
-    type Value TestStore = Int
-
-    query ix (TestStore s _) = return (Map.lookup ix s)
-
-    store ix xs term (TestStore s c) =
-        return . (\m -> TestStore m c) . fst $
-        Fold.foldl' (\(m, ixx) x -> (Map.insert ixx (x,term) m, ixx+1))
-          (s, ix) xs
-
-    commit ix (TestStore s _) = return (TestStore s ix)
-
-    truncate ix (TestStore s c) = return $ TestStore
-        (fst (Map.split ix s)) (min ix c)
-
-testQuery :: CS.Index -> TestStore -> Maybe (Int, CS.Term)
-testQuery ix ts = runIdentity $ CS.query ix ts
-
-testStore :: CS.Index -> [Int] -> CS.Term -> TestStore -> TestStore
-testStore ix xs term ts = runIdentity $ CS.store ix xs term ts
-
-testCommit :: CS.Index -> TestStore -> TestStore
-testCommit ix ts = runIdentity $ CS.commit ix ts
-
-testTruncate :: CS.Index -> TestStore -> TestStore
-testTruncate ix ts = runIdentity $ CS.truncate ix ts
--}
-
 ----------------------------------------------------------------------
-
-{-
-testState :: Free (CS.LogStoreF [] Int) a -> CS.Index -> Maybe (Int, CS.Term)
-testState f ix = flip evalState empty $ do
-    runTestStore (f >> CS.end')
-    TestStore s _c <- get
-    return $ Map.lookup ix s
-    -}
 
 runTestStore :: (MonadState TestStore m, Fold.Foldable t)
              => Free (CS.LogStoreF t Int) () -> m ()
